93a2b627deaaa86bb9c8eeb7813a0b6088ec2b91
Qmemorais <qmemorais@gmail.com>
2022-01-17 17:15:51 +0200

    Update GetContent
    Create empty functions to split code

--- Test_URLS/FindURL.cs
+++ Test_URLS/FindURL.cs
@@ -1,10 +1,46 @@
-﻿namespace Test_URLS
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test_URLS
 {
     internal class FindURL
     {
         public void GetContent(string url)
         {
+            var htmlScan = new List<string>();
+            var htmlSitemap = new List<string>();
+            try
+            {
 
+            }
+            catch(Exception e)
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+
+        private List<string> ScanWebPages(List<string> htmlScan)
+        {
+            return htmlScan;
+        }
+
+        private List<string> ScanExistSitemap(List<string> htmlSitemap)
+        {
+            return htmlSitemap;
+        }
+
+        private bool IsPageHTML(string url)
+        {
+            return false;
+        }
+
+        private void OutputData()
+        {
+                
         }
     }
 }
