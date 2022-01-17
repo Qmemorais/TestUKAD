5218acaf01f8f9f80510d166ecff00b632f661dd
Qmemorais <qmemorais@gmail.com>
2022-01-17 22:39:48 +0200

    Update Project
    Update method to scan web-URL from pages

--- Test_URLS/FindURL.cs
+++ Test_URLS/FindURL.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -46,28 +47,69 @@ namespace Test_URLS
         private List<string> ScanWebPages(List<string> htmlScan)
         {
             //get main page to find only url from website
-            string firstUrl = getMainURL(htmlScan[0]);
-            List<string> AllPages = new List<string>();
-            AllPages.Add(htmlScan[0]);
-            string Href = string.Format(@"href\s*=\s*(?:[""'](?<1>[^""']*){0}[""']|(?<1>\S+))", firstUrl);
-            Regex r = new Regex(Href, RegexOptions.IgnoreCase | RegexOptions.Compiled,
+            var firstUrl = getMainURL(htmlScan[0]);
+            var scannedPages = new List<string>
+            {
+                htmlScan[0]
+            };
+            var href = @"href\s*=\s*(?:[""'](?<1>[^""']*)[""']|(?<1>\S+))";
+            var reg = new Regex(href, RegexOptions.IgnoreCase | RegexOptions.Compiled,
                                             TimeSpan.FromSeconds(1));
 
             for(int i = 0; i < htmlScan.Count; i++)
             {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(htmlScan[i]);
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-                StreamReader read = new StreamReader(response.GetResponseStream(), Encoding.Default, true, 8192);
-                string HTMLtxt = read.ReadToEnd();
+                var request = (HttpWebRequest)WebRequest.Create(htmlScan[i]);
+                var response = (HttpWebResponse)request.GetResponse();
+                var read = new StreamReader(response.GetResponseStream(), Encoding.Default, true, 8192);
+                var HTMLtxt = read.ReadToEnd();
                 response.Close();
-                Match match = r.Match(HTMLtxt);
-                List<string> matches = new List<string>();
-                while (match.Success)
-                {
-                    matches.Add(match.Value);
-                }
-            }
 
+                var match = reg.Match(HTMLtxt);
+                var matches = new List<string>();
+
+                while (match.Success)
+                {   //delete part "href=''"
+                    var urlFromMatch = match.Value.Substring(6);
+
+                    if (!urlFromMatch.Contains("http"))
+                    {
+                        //if we have href="/index.html"
+                        urlFromMatch = firstUrl + urlFromMatch;
+                        if (urlFromMatch.IndexOf(firstUrl + "/") == -1)
+                            urlFromMatch = urlFromMatch.Insert(firstUrl.Length, "/");
+                    }
+
+                    if (urlFromMatch.Contains(firstUrl))
+                    {   //delete \" at the end of string
+                        var length = urlFromMatch.LastIndexOf("\"");
+
+                        if (length != -1)
+                            urlFromMatch = urlFromMatch.Substring(0, length);
+
+                        matches.Add(urlFromMatch);
+                    }
+
+                    match = match.NextMatch();
+                }
+                //scan only pages that doesn`t looking for before
+                matches = matches.Except(scannedPages).ToList();
+                scannedPages.AddRange(matches);
+
+                if (matches.Count!=0)
+                    for(int k = 0; k < matches.Count; k++)
+                    {
+                        //part of existing pages doesn`t interesting
+                        if(matches[k].IndexOf("#") == -1)
+                        {
+                            //if difference is only http or https
+                            var existingPages = htmlScan.Any(web => web.IndexOf(matches[k].Substring(5)) != -1);
+                            if (!existingPages || matches[k] == firstUrl + "/")
+                                if (IsPageHTML(matches[k]))
+                                    //if this page is text/html then add to scanList
+                                    htmlScan.Add(matches[k]);
+                        }
+                    }
+            }
             return htmlScan;
         }
 
@@ -101,10 +143,10 @@ namespace Test_URLS
 
         private string getMainURL(string url)
         {
-            int firstSymbolAfter = url.IndexOf("//")+2;
             int lastSymbolBefore = url.IndexOf("/", 8);
+
             if (lastSymbolBefore != -1)
-                url = url.Substring(firstSymbolAfter, lastSymbolBefore - firstSymbolAfter);
+                url = url.Substring(0, lastSymbolBefore);
             return url;
         }
     }
