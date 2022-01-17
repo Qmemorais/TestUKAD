9026821ec2adc9f74aa7ad850a7dee2a441574fa
Qmemorais <qmemorais@gmail.com>
2022-01-17 16:56:26 +0200

    Create Project

---
+++ Test_URLS/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Test_URLS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter URL: ");
+            string url = Console.ReadLine();
+            
+        }
+    }
+}
