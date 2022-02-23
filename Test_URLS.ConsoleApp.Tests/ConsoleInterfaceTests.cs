33d402206a6e2db69f02d1f1f5f5d9e0939c6ebe
Daria Burtseva <qmemorais@gmail.com>
2022-02-23 07:46:46 -0800

    Load Moq for tests. Create tests for ConsoleApp
    load Moq to project of UnitTests. create tests if input value is empty and tests to catch exceptions (Forbiden, not found, incorrect type of url). create one test with correct values and return type

--- Test_URLS.ConsoleApp.Tests/ConsoleInterfaceTests.cs
+++ Test_URLS.ConsoleApp.Tests/ConsoleInterfaceTests.cs
@@ -1,18 +1,108 @@
 using NUnit.Framework;
+using Moq;
+using Test_URLS.urlLogic;
+using System.Net;
+using System.Collections.Generic;
 
 namespace Test_URLS.ConsoleApp.Tests
 {
     public class Tests
     {
+        private Mock<IConsoleInOut> _consoleInOut;
+        private Mock<MainLogic> _mainLogic;
+        private ConsoleInterface _consoleInterface;
+        private readonly string _writeLineOutput = "Press <Enter>";
+
         [SetUp]
         public void Setup()
         {
+            _consoleInOut = new Mock<IConsoleInOut>();
+            _mainLogic = new Mock<MainLogic>();
+            _consoleInterface = new ConsoleInterface(_consoleInOut.Object,_mainLogic.Object);
         }
 
         [Test]
-        public void Test1()
+        public void Start_InputEmptyString_ReturnExceptionMessage()
         {
-            Assert.Pass();
+            //arrange
+            var readLine = "";
+            var writeLineRes = "Invalid URI: The URI is empty.";
+
+            _consoleInOut
+                .Setup(x => x.Read())
+                .Returns(readLine);
+            _mainLogic
+                .Setup(x => x.GetResults(""))
+                .Throws(new WebException("Invalid URI: The URI is empty."));
+            //act
+            _consoleInterface.Start();
+            //assert
+            _consoleInOut.Verify(x => x.Write(writeLineRes), Times.Once);
+            _consoleInOut.Verify(x => x.Write(_writeLineOutput), Times.Once);
+            _consoleInOut.Verify(x => x.Read(), Times.Exactly(2));
+        }
+
+        [Test]
+        public void Start_InputCorrectURL_ReturnExceptionMessageForbiden()
+        {
+            //arrange
+            var readLine = "";
+            var writeLineRes = "403 - Forbidden";
+
+            _consoleInOut
+                .Setup(x => x.Read())
+                .Returns(readLine);
+            _mainLogic
+                .Setup(x => x.GetResults(""))
+                .Throws(new WebException("403 - Forbidden"));
+            //act
+            _consoleInterface.Start();
+            //assert
+            _consoleInOut.Verify(x => x.Write(writeLineRes), Times.Once);
+            _consoleInOut.Verify(x => x.Write(_writeLineOutput), Times.Once);
+            _consoleInOut.Verify(x => x.Read(), Times.Exactly(2));
+        }
+
+        [Test]
+        public void Start_InputCorrectURL_ReturnExceptionMessageNotFound()
+        {
+            //arrange
+            var readLine = "";
+            var writeLineRes = "404 - NotFound";
+
+            _consoleInOut
+                .Setup(x => x.Read())
+                .Returns(readLine);
+            _mainLogic
+                .Setup(x => x.GetResults(""))
+                .Throws(new WebException("404 - NotFound"));
+            //act
+            _consoleInterface.Start();
+            //assert
+            _consoleInOut.Verify(x => x.Write(writeLineRes), Times.Once);
+            _consoleInOut.Verify(x => x.Write(_writeLineOutput), Times.Once);
+            _consoleInOut.Verify(x => x.Read(), Times.Exactly(2));
+        }
+
+        [Test]
+        public void Start_InputCorrectURL_ReturnCorectCountWriteLine()
+        {
+            //arrange
+            var readLine = "";
+            var writeLineRes = "this is Url";
+            IEnumerable<string> expectRes = new List<string> { writeLineRes, writeLineRes, writeLineRes };
+
+            _consoleInOut
+                .Setup(x => x.Read())
+                .Returns(readLine);
+            _mainLogic
+                .Setup(x => x.GetResults(""))
+                .Returns(expectRes);
+            //act
+            _consoleInterface.Start();
+            //assert
+            _consoleInOut.Verify(x => x.Write(writeLineRes), Times.Exactly(3));
+            _consoleInOut.Verify(x => x.Write(_writeLineOutput), Times.Once);
         }
     }
 }
