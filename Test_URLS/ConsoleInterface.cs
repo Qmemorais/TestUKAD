a2cea7f84a78c3004bb0de19aa2ff04471c5f946
Daria Burtseva <qmemorais@gmail.com>
2022-02-23 02:48:05 -0800

    Update Class ConsoleInterface
    create method to call logic.Main() of scan url. create constructor of class to use it in UnitTests with Mock

--- Test_URLS/ConsoleInterface.cs
+++ Test_URLS/ConsoleInterface.cs
@@ -1,6 +1,32 @@
-﻿namespace Test_URLS.ConsoleApp
+﻿using Test_URLS.urlLogic;
+
+namespace Test_URLS.ConsoleApp
 {
     class ConsoleInterface
     {
+        private IConsoleInOut _consoleInOut = new ConsoleInOut();
+        private MainLogic _logic = new MainLogic();
+
+        public ConsoleInterface(IConsoleInOut consoleInOut)
+        {
+            _consoleInOut = consoleInOut;
+        }
+
+        public ConsoleInterface()
+        {
+
+        }
+
+        public void Start()
+        {
+            _consoleInOut.Write("Enter URL: ");
+                string urlToScan = _consoleInOut.Read();
+
+            var resultDictionary = _logic.GetResults(urlToScan);
+            _consoleInOut.Write(resultDictionary);
+
+            _consoleInOut.Write("Press <Enter>");
+            _consoleInOut.Read();
+        }
     }
 }
