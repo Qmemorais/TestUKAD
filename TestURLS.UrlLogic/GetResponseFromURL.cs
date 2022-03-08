43438db8af7269942769ae42188f60692a2062f1
Qmemorais <qmemorais@gmail.com>
2022-03-08 15:52:13 +0200

    Create GetResponseFromURL.cs
    Create class to call WebRequest and WebResponse. make return type HttpWebResponse

---
+++ TestURLS.UrlLogic/GetResponseFromURL.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace TestURLS.UrlLogic
+{
+    public class GetResponseFromURL
+    {
+        public virtual HttpWebResponse GetResponse(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Timeout = 10000;
+            return (HttpWebResponse)request.GetResponse();
+        }
+    }
+}
