35e3831c059ae7f377241c440aa432daaf38a14b
Qmemorais <qmemorais@gmail.com>
2022-03-09 18:26:29 +0200

    Update MainLogic.cs
    remove try/catch

--- TestURLS.urlLogic/MainLogic.cs
+++ TestURLS.urlLogic/MainLogic.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Net;
 
 namespace TestURLS.UrlLogic
@@ -8,11 +7,11 @@ namespace TestURLS.UrlLogic
     {
         private readonly LogicScanByHTML _scanByHTML = new LogicScanByHTML();
         private readonly LogicScanBySitemap _scanBySitemap = new LogicScanBySitemap();
-        private readonly GetResponseFromURL _getResponse = new GetResponseFromURL();
+        private readonly GetRequestFromURL _getResponse = new GetRequestFromURL();
         private readonly OutputList _outputList = new OutputList();
 
         public MainLogic(LogicScanByHTML scanByHTML, LogicScanBySitemap scanBySitemap,
-            GetResponseFromURL getResponse, OutputList outputList)
+            GetRequestFromURL getResponse, OutputList outputList)
         {
             _scanByHTML = scanByHTML;
             _scanBySitemap = scanBySitemap;
@@ -28,33 +27,20 @@ namespace TestURLS.UrlLogic
             var htmlScan = new List<string>();
             var htmlSitemap = new List<string>();
             IEnumerable<string> stringToType = htmlScan;
+            var statusCode = _getResponse.GetStatusCode(url);
 
-            try
+            if (statusCode == HttpStatusCode.OK)
             {
-                //try open url
-                var response = _getResponse.GetResponse(url);
                 //if OK add this url to list and work
                 htmlScan.Add(url);
                 //scan all exist pages on web
                 htmlScan = _scanByHTML.ScanWebPages(htmlScan);
                 //find sitemap and if yes: scan
-                htmlSitemap = _scanBySitemap.ScanExistSitemap(url, htmlSitemap);
+                htmlSitemap = _scanBySitemap.VerifyExistStitemap(url, htmlSitemap);
                 //
                 stringToType = _outputList.OutputTables(htmlScan, htmlSitemap);
             }
-            catch (WebException e)
-            {
-                //catch 403 and 404 errorsdf
-                WebExceptionStatus status = e.Status;
-
-                if (status == WebExceptionStatus.ProtocolError)
-                {
-                    HttpWebResponse httpResponse = (HttpWebResponse)e.Response;
-                    stringToType = 
-                        (IEnumerable<string>)(IEnumerable)$"{(int)httpResponse.StatusCode} - {httpResponse.StatusCode}";
-                }
-            }
-                return stringToType;
+            return stringToType;
         }
     }
 }
