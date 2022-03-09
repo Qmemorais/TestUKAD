e2896bd2f255a93875a66295f35016e432d99238
Qmemorais <qmemorais@gmail.com>
2022-03-09 18:25:41 +0200

    Update LogicScanBySitemap.cs
    remove try/catch. remove response method to Request class

--- TestURLS.urlLogic/LogicScanBySitemap.cs
+++ TestURLS.urlLogic/LogicScanBySitemap.cs
@@ -1,17 +1,15 @@
 ﻿using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
 using System.Xml;
 
 namespace TestURLS.UrlLogic
 {
     public class LogicScanBySitemap
     {
-        private readonly GetResponseFromURL _getResponse = new GetResponseFromURL();
+        private readonly GetRequestFromURL _getResponse = new GetRequestFromURL();
         private readonly GetSettingFromURL _settingsOfURL = new GetSettingFromURL();
 
-        public LogicScanBySitemap(GetResponseFromURL getResponse, GetSettingFromURL settingsOfURL)
+        public LogicScanBySitemap(GetRequestFromURL getResponse, GetSettingFromURL settingsOfURL)
         {
             _getResponse = getResponse;
             _settingsOfURL = settingsOfURL;
@@ -19,29 +17,23 @@ namespace TestURLS.UrlLogic
 
         public LogicScanBySitemap() { }
 
-        public virtual List<string> ScanExistSitemap(string url, List<string> htmlSitemap)
+        public virtual List<string> VerifyExistStitemap(string url, List<string> htmlSitemap)
         {
             var firstUrl = _settingsOfURL.GetMainURL(url);
-            try
+            //try open page/sitemap.xml
+            var isSitemapExist = _getResponse.GetContentType(firstUrl + "/sitemap.xml");
+            if (isSitemapExist)
             {
-                //try open page/sitemap.xml
-                var isSitemapExist = _settingsOfURL.IsPageHTML(firstUrl + "/sitemap.xml");
                 htmlSitemap = ScanSitemap(firstUrl + "/sitemap.xml");
             }
-            catch
+            else
             {
                 //if it doesn`t exist try to find url of sitemap
-                var response = _getResponse.GetResponse(firstUrl + "/robots.txt");
-                var reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(1251));
-                string line;
-                while ((line = reader.ReadLine()) != null)
+                var urlToFindWitemap = _getResponse.GetSitemapFromURL(firstUrl + "/robots.txt");
+                if (urlToFindWitemap != "")
                 {
-                    if (line.IndexOf("Sitemap: ") != -1)
-                    {
-                        htmlSitemap = ScanSitemap(line[9..]);
-                    }
+                    htmlSitemap = ScanSitemap(urlToFindWitemap);
                 }
-                response.Close();
             }
             return htmlSitemap;
         }
@@ -51,19 +43,19 @@ namespace TestURLS.UrlLogic
             var htmlSitemap = new List<string>();
             //create value to get xml-document and data from
             var xDoc = new XmlDocument();
-            try
+            xDoc.Load(sitemapURL);
+            XmlElement xRoot = xDoc.DocumentElement;
+            foreach (XmlNode xnode in xRoot)
             {
-                xDoc.Load(sitemapURL);
-                XmlElement xRoot = xDoc.DocumentElement;
-                foreach (XmlNode xnode in xRoot)
-                    foreach (XmlNode childnode in xnode.ChildNodes)
-                        if (childnode.Name == "loc")
-                            htmlSitemap.Add(childnode.InnerText);
-                htmlSitemap = htmlSitemap.Distinct().ToList();
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    if (childnode.Name == "loc")
+                    {
+                        htmlSitemap.Add(childnode.InnerText);
+                    }    
+                }
             }
-            catch
-            {
-            }
+            htmlSitemap = htmlSitemap.Distinct().ToList();
             return htmlSitemap;
         }
     }
