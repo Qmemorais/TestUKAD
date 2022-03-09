71790dd88b367ae1d0e5a13b3bc1fcec29915929
Qmemorais <qmemorais@gmail.com>
2022-03-09 18:23:02 +0200

    Update GetSettingFromURL.cs
    remove method to get ContentType to class which work with response

--- TestURLS.urlLogic/GetSettingFromURL.cs
+++ TestURLS.urlLogic/GetSettingFromURL.cs
@@ -2,31 +2,6 @@
 {
     public class GetSettingFromURL
     {
-        private readonly GetResponseFromURL _getResponse = new GetResponseFromURL();
-
-        public GetSettingFromURL(GetResponseFromURL _response)
-        {
-            _getResponse = _response;
-        }
-
-        public GetSettingFromURL() { }
-
-        public virtual bool IsPageHTML(string url)
-        {
-            try
-            {
-                //find text/html pages
-                var response = _getResponse.GetResponse(url);
-                var contentType = response.ContentType.IndexOf("text/html") != -1;
-                response.Close();
-                return contentType;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
         public virtual string GetMainURL(string url)
         {
             int lastSymbolBefore = url.IndexOf("/", 8);
