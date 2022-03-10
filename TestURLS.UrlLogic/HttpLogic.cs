a534ebee728e48848c1a78d75fce88b2bb1b1479
Qmemorais <qmemorais@gmail.com>
2022-03-10 18:52:56 +0200

    Rename class with Response methods

--- TestURLS.UrlLogic/HttpLogic.cs
+++ TestURLS.UrlLogic/HttpLogic.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace TestURLS.UrlLogic
 {
-    public class GetRequestFromURL
+    public class HttpLogic
     {
         public virtual HttpStatusCode GetStatusCode(string url)
         {
@@ -21,55 +21,48 @@ namespace TestURLS.UrlLogic
             }
         }
 
-        public virtual HttpWebResponse GetResponse(string url)
-        {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            return (HttpWebResponse)request.GetResponse();
-        }
-
         public virtual bool GetContentType(string url)
         {
-            var contentType = false;
+            bool contentType = false;
             //find text/html pages
             if (GetStatusCode(url) == HttpStatusCode.OK)
             {
-                var response = GetResponse(url).ContentType;
-                contentType = response.IndexOf("text/html") != -1;
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                var response = (HttpWebResponse)request.GetResponse();
+                contentType = response.ContentType.Equals("text/html");
+                response.Close();
             }
             return contentType;
         }
 
         public virtual string GetBodyFromURL(string url)
         {
-            var HTMLtxt = "";
+            var HTMLtxt = string.Empty;
             if (GetStatusCode(url) == HttpStatusCode.OK)
             {
-                var response = GetResponse(url);
-                var read = new StreamReader(response.GetResponseStream(), Encoding.Default, true, 8192);
-                HTMLtxt = read.ReadToEnd();
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                var response = (HttpWebResponse)request.GetResponse();
+                var encoding = response.CharacterSet;
+                var reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(encoding));
+                HTMLtxt = reader.ReadToEnd();
                 response.Close();
             }
             return HTMLtxt;
         }
 
-        public virtual string GetSitemapFromURL(string url)
+        public virtual StreamReader GetSitemapFromURL(string url)
         {
-            string line;
-            string sitemap = "";
+            StreamReader reader = null;
             if (GetStatusCode(url) == HttpStatusCode.OK)
             {
-                var response = GetResponse(url);
-                var reader = new StreamReader(response.GetResponseStream(), Encoding.Default, true, 8192);
-                while ((line = reader.ReadLine()) != null)
-                {
-                    if (line.IndexOf("Sitemap: ") != -1)
-                    {
-                        sitemap = line[9..];
-                    }
-                }
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                var response = (HttpWebResponse)request.GetResponse();
+                var encoding = response.CharacterSet;
+                reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(encoding));
                 response.Close();
+                return reader;
             }
-            return sitemap;
+            return reader;
         }
     }
 }
