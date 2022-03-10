d5c911f0ca6e0b1fdfe3675df55ee1166d56dc98
Qmemorais <qmemorais@gmail.com>
2022-03-10 18:52:28 +0200

    Relocate Output class
    remove class to Console project because this is output strings

---
+++ TestURLS.ConsoleApp/OutputToConsole.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using TestURLS.UrlLogic;
+
+namespace TestURLS.ConsoleApp
+{
+    public class OutputToConsole
+    {
+        private readonly HttpLogic _getResponse = new HttpLogic();
+        private readonly MainLogic _logic = new MainLogic();
+        private readonly IConsoleInOut _consoleInOut = new ConsoleInOut();
+
+        public OutputToConsole(IConsoleInOut consoleInOut, MainLogic logic,
+            HttpLogic getResponse)
+        {
+            _consoleInOut = consoleInOut;
+            _logic = logic;
+            _getResponse = getResponse;
+        }
+
+        public OutputToConsole() { }
+
+        public virtual void Write(List<string> UrlsFromSitemap,
+            List<string> UrlFromWeb)
+        {
+            if (UrlsFromSitemap.Count == 0)
+            {
+                OutputTime(UrlFromWeb);
+                _consoleInOut.Write("Urls(html documents) found after crawling a website: " + UrlFromWeb.Count);
+            }
+            else
+            {
+
+                _consoleInOut.Write("Urls FOUNDED IN SITEMAP.XML but not founded after crawling a web site");
+                OutputURLS(_logic.GetExistLists(UrlsFromSitemap, UrlFromWeb));
+                _consoleInOut.Write("Urls FOUNDED BY CRAWLING THE WEBSITE but not in sitemap.xml");
+                OutputURLS(_logic.GetExistLists(UrlFromWeb, UrlsFromSitemap));
+                _consoleInOut.Write("Urls FOUNDED BY CRAWLING THE WEBSITE AND SITEMAP.XML");
+                OutputTime(UrlFromWeb
+                    .Union(_logic.GetExistLists(UrlsFromSitemap, UrlFromWeb))
+                    .ToList());
+                _consoleInOut.Write("Urls(html documents) found after crawling a website: " + UrlFromWeb.Count);
+                _consoleInOut.Write("Urls found in sitemap: " + UrlsFromSitemap.Count);
+            }
+
+        }
+
+        protected virtual void OutputTime(List<string> html)
+        {
+            var urlWithTime = new Dictionary<string, int>();
+
+            foreach (string url in html)
+            {
+                //get time of request
+                Stopwatch sw = Stopwatch.StartNew();
+                var response = _getResponse.GetStatusCode(url);
+                sw.Stop();
+                var time = (int)sw.ElapsedMilliseconds;
+                urlWithTime.Add(url, time);
+            }
+
+            urlWithTime = urlWithTime.OrderBy(pair => pair.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+
+            var lengthURL = html.Max(x => x.Length) + 4;
+            _consoleInOut.Write(new string('_', lengthURL + 14));
+            _consoleInOut.Write(String.Format("{0}{1}|{2,-12}{3}", "|", "URL".PadRight(lengthURL, ' '), "Timing (ms)", "|"));
+            _consoleInOut.Write(new string('_', lengthURL + 14));
+
+            for (int i = 0; i < urlWithTime.Count; i++)
+            {
+                _consoleInOut.Write(String.Format("{0}{1}|{2,-12}{3}", "|", ((i + 1) + ") " + urlWithTime.ElementAt(i).Key).PadRight(lengthURL, ' '), urlWithTime.ElementAt(i).Value + "ms", "|"));
+                _consoleInOut.Write(new string('_', lengthURL + 14));
+            }
+        }
+
+        protected virtual void OutputURLS(List<string> html)
+        {
+            var lengthURL = html.Max(x => x.Length) + 4;
+            _consoleInOut.Write(new string('_', lengthURL + 2));
+            _consoleInOut.Write(String.Format("{0}{1}{2}", "|", "URL".PadRight(lengthURL, ' '), "|"));
+            _consoleInOut.Write(new string('_', lengthURL + 2));
+
+            for (int i = 0; i < html.Count; i++)
+            {
+                _consoleInOut.Write(String.Format("{0}{1}|", "|", ((i + 1) + ") " + html[i]).PadRight(lengthURL, ' ')));
+                _consoleInOut.Write(new string('_', lengthURL + 2));
+            }
+        }
+
+    }
+}
