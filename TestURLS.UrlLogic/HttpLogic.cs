e7e5df769b860ca0181d9ce4b5464dd758343581
Qmemorais <qmemorais@gmail.com>
2022-03-11 13:22:59 +0200

    Update HttpLogic.cs
    remove method which already exist in class

--- TestURLS.UrlLogic/HttpLogic.cs
+++ TestURLS.UrlLogic/HttpLogic.cs
@@ -6,63 +6,43 @@ namespace TestURLS.UrlLogic
 {
     public class HttpLogic
     {
-        public virtual HttpStatusCode GetStatusCode(string url)
-        {
-            try
-            {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-                var response = (HttpWebResponse)request.GetResponse();
-                return response.StatusCode;
-            }
-            catch(WebException e)
-            {
-                HttpWebResponse httpResponse = (HttpWebResponse)e.Response;
-                return httpResponse.StatusCode;
-            }
-        }
-
         public virtual bool GetContentType(string url)
         {
             bool contentType = false;
             //find text/html pages
-            if (GetStatusCode(url) == HttpStatusCode.OK)
+            try
             {
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
                 var response = (HttpWebResponse)request.GetResponse();
-                contentType = response.ContentType.Equals("text/html");
+                contentType = response.ContentType == ("text/html");
                 response.Close();
+                return contentType;
+            }
+            catch
+            {
+                //TODO: Add logger here
             }
             return contentType;
         }
 
-        public virtual string GetBodyFromURL(string url)
+        public virtual string GetSitemapFromURL(string url)
         {
-            var HTMLtxt = string.Empty;
-            if (GetStatusCode(url) == HttpStatusCode.OK)
+            var HtmlTxt = string.Empty;
+            try
             {
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
                 var response = (HttpWebResponse)request.GetResponse();
                 var encoding = response.CharacterSet;
                 var reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(encoding));
-                HTMLtxt = reader.ReadToEnd();
+                HtmlTxt = reader.ReadToEnd();
                 response.Close();
+                return HtmlTxt;
             }
-            return HTMLtxt;
-        }
-
-        public virtual StreamReader GetSitemapFromURL(string url)
-        {
-            StreamReader reader = null;
-            if (GetStatusCode(url) == HttpStatusCode.OK)
+            catch
             {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-                var response = (HttpWebResponse)request.GetResponse();
-                var encoding = response.CharacterSet;
-                reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(encoding));
-                response.Close();
-                return reader;
+                //TODO: Add logger here
             }
-            return reader;
+            return HtmlTxt;
         }
     }
 }
