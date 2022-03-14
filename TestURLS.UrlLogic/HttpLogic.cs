d7dc7a9943de49f5beb72576d561fb3a1228b809
Qmemorais <qmemorais@gmail.com>
2022-03-14 14:01:58 +0200

    Update HttpLogic.cs
    remove method to get content type. add more "if" o understand is link is good

--- TestURLS.UrlLogic/HttpLogic.cs
+++ TestURLS.UrlLogic/HttpLogic.cs
@@ -6,43 +6,27 @@ namespace TestURLS.UrlLogic
 {
     public class HttpLogic
     {
-        public virtual bool GetContentType(string url)
-        {
-            bool contentType = false;
-            //find text/html pages
-            try
-            {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-                var response = (HttpWebResponse)request.GetResponse();
-                contentType = response.ContentType == ("text/html");
-                response.Close();
-                return contentType;
-            }
-            catch
-            {
-                //TODO: Add logger here
-            }
-            return contentType;
-        }
 
-        public virtual string GetSitemapFromURL(string url)
+        public virtual string GetBodyFromUrl(string url)
         {
-            var HtmlTxt = string.Empty;
+            var bodyTxt = string.Empty;
+
             try
             {
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
                 var response = (HttpWebResponse)request.GetResponse();
                 var encoding = response.CharacterSet;
                 var reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(encoding));
-                HtmlTxt = reader.ReadToEnd();
+
+                bodyTxt = reader.ReadToEnd();
+
                 response.Close();
-                return HtmlTxt;
             }
             catch
             {
                 //TODO: Add logger here
             }
-            return HtmlTxt;
+            return bodyTxt;
         }
     }
 }
