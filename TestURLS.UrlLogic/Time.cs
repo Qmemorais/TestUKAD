5df9d44605d2007f8d67d3ce22ef39c1db5a8ead
Qmemorais <qmemorais@gmail.com>
2022-03-14 13:47:51 +0200

    Create Time.cs
    create class and method to get time from response and save it

---
+++ TestURLS.UrlLogic/Time.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+
+namespace TestURLS.UrlLogic
+{
+    public class Time
+    {
+        public virtual List<UrlTimeModel> GetLinksWithTime(List<string> html)
+        {
+            List<UrlTimeModel> urlWithTime = new List<UrlTimeModel>();
+
+            foreach (string url in html)
+            {
+                //get time of request
+                Stopwatch timeToResponse = Stopwatch.StartNew();
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                var response = (HttpWebResponse)request.GetResponse();
+
+                timeToResponse.Stop();
+
+                var time = timeToResponse.ElapsedMilliseconds;
+                UrlTimeModel model = new UrlTimeModel()
+                {
+                    Link = url,
+                    TimeOfResponse = time
+                };
+
+                urlWithTime.Add(model);
+            }
+
+            return urlWithTime;
+        }
+    }
+}
