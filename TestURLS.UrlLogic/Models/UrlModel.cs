b589a01ac62424e94fe5a6902cafb483400f5b5d
Qmemorais <qmemorais@gmail.com>
2022-03-15 16:22:20 +0200

    Remove project Model. Create folder

---
+++ TestURLS.UrlLogic/Models/UrlModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TestURLS.UrlLogic.Models
+{
+    public class UrlModel
+    {
+        public string Link { get; set; }
+        public bool IsSitemap { get; set; }
+        public bool IsWeb { get; set; }
+
+    }
+}
