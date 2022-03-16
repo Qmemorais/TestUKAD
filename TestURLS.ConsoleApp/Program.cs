4fd677086a283e0113f444f65d97a9a0a8538fc1
Qmemorais <qmemorais@gmail.com>
2022-03-16 18:58:51 +0200

    Create method to add DI
    add reference on library to use DI
    create method to add coupling between interfaces and classes

--- TestURLS.ConsoleApp/Program.cs
+++ TestURLS.ConsoleApp/Program.cs
@@ -1,12 +1,38 @@
-﻿namespace TestURLS.ConsoleApp
+﻿using Microsoft.Extensions.DependencyInjection;
+using TestURLS.ConsoleApp.Interfaces;
+using TestURLS.UrlLogic;
+using TestURLS.UrlLogic.Interfaces;
+
+namespace TestURLS.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            LogicToConsole consoleInterface = new LogicToConsole();
+            LogicToConsole logicToConsole;
+            var services = ConfigureServices();
+            var serviceProvider = services.BuildServiceProvider();
 
-            consoleInterface.Start();
+            logicToConsole = serviceProvider.GetService<LogicToConsole>();
+            logicToConsole.Start();
+        }
+
+        private static IServiceCollection ConfigureServices()
+        {
+            var services = new ServiceCollection();
+            services
+                    .AddScoped<IConsoleInOut, ConsoleInOut>()
+                    .AddScoped<LogicToConsole>()
+                    .AddScoped<IOutputToConsole, OutputToConsole>()
+
+                    .AddScoped<IHttpLogic, HttpLogic>()
+                    .AddScoped<ILogicScanByHtml, LogicScanByHtml>()
+                    .AddScoped<ILogicScanBySitemap, LogicScanBySitemap>()
+                    .AddScoped<IMainLogic, MainLogic>()
+                    .AddScoped<ITimeTracker, TimeTracker>()
+                    .AddScoped<IUrlSettings, UrlSettings>();
+
+            return services;
         }
     }
 }
