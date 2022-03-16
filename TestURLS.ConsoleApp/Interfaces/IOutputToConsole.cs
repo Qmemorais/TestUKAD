1fcc192ec672dfeea5d498fdd9b20b90370ac35f
Qmemorais <qmemorais@gmail.com>
2022-03-16 18:55:41 +0200

    Create IOutputToConsole.cs
    create interface for OutputToConsole public methods

---
+++ TestURLS.ConsoleApp/Interfaces/IOutputToConsole.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TestURLS.UrlLogic.Models;
+
+namespace TestURLS.ConsoleApp.Interfaces
+{
+    public interface IOutputToConsole
+    {
+        void WriteLinksWithoutTime(List<UrlModel> allLinksFromSitemapAndScan);
+        void WriteLinksWithTime(List<UrlModelWithResponse> linksWithResponseTime);
+        void WriteCountLinks(List<UrlModel> allLinksFromSitemapAndScan);
+    }
+}
