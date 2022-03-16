c752f7d5fdc774a6fc70f504d519302e2e5ea885
Qmemorais <qmemorais@gmail.com>
2022-03-16 18:56:23 +0200

    Create ILogicScanByHtml.cs
    create interface for LogicScanBuHtml public methods

---
+++ TestURLS.UrlLogic/Interfaces/ILogicScanByHtml.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using TestURLS.UrlLogic.Models;
+
+namespace TestURLS.UrlLogic.Interfaces
+{
+    public interface ILogicScanByHtml
+    {
+        IEnumerable<UrlModel> GetUrlsFromScanPages(string url);
+    }
+}
