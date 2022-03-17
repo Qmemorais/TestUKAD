b14d9921b53ff169277a559aea115773c71aa753
Qmemorais <qmemorais@gmail.com>
2022-03-17 22:37:03 +0200

    Create tests for UrlSetting.cs
    create tests for all public methods in class

---
+++ TestURLS.UrlLogic.Tests/UrlSettingsTests.cs
@@ -0,0 +1,87 @@
+using NUnit.Framework;
+
+namespace TestURLS.UrlLogic.Tests
+{
+    public class UrlSettingsTests
+    {
+        private UrlSettings _urlSettings;
+
+        [SetUp]
+        public void Setup()
+        {
+            _urlSettings = new UrlSettings();
+        }
+
+        [Test]
+        public void GetDomainName_DomainName_ExistLink()
+        {
+            var url = "https://test.crawler.com";
+            //
+            var results = _urlSettings.GetDomainName(url);
+            //
+            Assert.AreEqual(url, results);
+
+        }
+
+        [Test]
+        public void GetDomainName_Url_DomainNameFromUrl()
+        {
+            var url = "https://test.crawler.com/Info/";
+            var expectedRsult = "https://test.crawler.com";
+            //
+            var results = _urlSettings.GetDomainName(url);
+            //
+            Assert.AreEqual(expectedRsult, results);
+        }
+
+        [Test]
+        public void GetValidUrl_ValidUrl_Url()
+        {
+            //
+            var url = "https://test.crawler.com/Info/";
+            var domainName = "https://test.crawler.com";
+            //
+            var result = _urlSettings.GetValidUrl(url, domainName);
+            //
+            Assert.AreEqual(url, result);
+        }
+
+        [Test]
+        public void GetValidUrl_WrongUrl_NewUrl()
+        {
+            //
+            var url = "/Info/";
+            var domainName = "https://test.crawler.com";
+            var expectedResult = "https://test.crawler.com/Info/";
+            //
+            var result = _urlSettings.GetValidUrl(url, domainName);
+            //
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void GetUrlLikeWeb_UncorrectUrl_NewUrl()
+        {
+            //
+            var url = "https://www.test.crawler.com/Info/";
+            var domainName = "https://test.crawler.com";
+            var expectedResult = "https://test.crawler.com/Info/";
+            //
+            var result = _urlSettings.GetUrlLikeFromWeb(url, domainName);
+            //
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void GetUrlLikeWeb_CorrectUrl_ExisttUrl()
+        {
+            //
+            var url = "https://test.crawler.com/Info/";
+            var domainName = "https://test.crawler.com";
+            //
+            var result = _urlSettings.GetUrlLikeFromWeb(url, domainName);
+            //
+            Assert.AreEqual(url, result);
+        }
+    }
+}
