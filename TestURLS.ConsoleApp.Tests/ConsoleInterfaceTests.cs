aa0684951de3a2f8140e00fc9e2e952205a075f8
Qmemorais <qmemorais@gmail.com>
2022-03-17 17:31:53 +0200

    Update ConsoleInterfaceTests.cs

--- TestURLS.ConsoleApp.Tests/ConsoleInterfaceTests.cs
+++ TestURLS.ConsoleApp.Tests/ConsoleInterfaceTests.cs
@@ -1,25 +1,31 @@
-using NUnit.Framework;
-using Moq;
-using TestURLS.UrlLogic;
-using System.Net;
 using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using Moq;
+using NUnit.Framework;
+using TestURLS.ConsoleApp.Interfaces;
+using TestURLS.UrlLogic.Interfaces;
+using TestURLS.UrlLogic.Models;
 
 namespace TestURLS.ConsoleApp.Tests
 {
-    public class Tests
+    public class LogicToConsoleTests
     {
         private Mock<IConsoleInOut> _consoleInOut;
-        private Mock<MainLogic> _mainLogic;
-        private ConsoleInterface _consoleInterface;
+        private Mock<IMainLogic> _mainLogic;
+        private Mock<IOutputToConsole> _outputToConsole;
+        private LogicToConsole _consoleInterface;
         private readonly string _writeLineOutput = "Press <Enter>";
 
         [SetUp]
         public void Setup()
         {
             _consoleInOut = new Mock<IConsoleInOut>();
-            _mainLogic = new Mock<MainLogic>();
-            _consoleInterface = new ConsoleInterface(_consoleInOut.Object,_mainLogic.Object);
+            _mainLogic = new Mock<IMainLogic>();
+            _outputToConsole = new Mock<IOutputToConsole>();
+            _consoleInterface = new LogicToConsole(
+                _consoleInOut.Object,
+                _mainLogic.Object,
+                _outputToConsole.Object);
         }
 
         [Test]
@@ -34,66 +40,59 @@ namespace TestURLS.ConsoleApp.Tests
                 .Returns(readLine);
             _mainLogic
                 .Setup(x => x.GetResults(""))
-                .Throws(new WebException("Invalid URI: The URI is empty."));
-            //act
-            _consoleInterface.Start();
+                .Throws(new WebException(writeLineRes));
             //assert
-            _consoleInOut.Verify(x => x.Write(writeLineRes), Times.Once);
-            _consoleInOut.Verify(x => x.Write(_writeLineOutput), Times.Once);
-            _consoleInOut.Verify(x => x.Read(), Times.Exactly(2));
+            Assert.Throws<WebException>(() => _consoleInterface.Start()).Message.Equals(writeLineRes);
         }
 
         [Test]
-        public void Start_InputCorrectURL_ReturnExceptionMessageForbiden()
+        public void Start_InputCorrectUrl_ReturnExceptionMessageForbiden()
         {
             //arrange
-            var readLine = "";
+            var readLine = "https://example.com/";
             var writeLineRes = "403 - Forbidden";
 
             _consoleInOut
                 .Setup(x => x.Read())
                 .Returns(readLine);
             _mainLogic
-                .Setup(x => x.GetResults(""))
-                .Throws(new WebException("403 - Forbidden"));
-            //act
-            _consoleInterface.Start();
+                .Setup(x => x.GetResults(readLine))
+                .Throws(new WebException(writeLineRes));
             //assert
-            _consoleInOut.Verify(x => x.Write(writeLineRes), Times.Once);
-            _consoleInOut.Verify(x => x.Write(_writeLineOutput), Times.Once);
-            _consoleInOut.Verify(x => x.Read(), Times.Exactly(2));
+            Assert.Throws<WebException>(() => _consoleInterface.Start()).Message.Equals(writeLineRes);
         }
 
         [Test]
-        public void Start_InputCorrectURL_ReturnExceptionMessageNotFound()
+        public void Start_InputCorrectUrl_ReturnExceptionMessageNotFound()
         {
             //arrange
-            var readLine = "";
+            var readLine = "https://example.com/";
             var writeLineRes = "404 - NotFound";
 
             _consoleInOut
                 .Setup(x => x.Read())
                 .Returns(readLine);
             _mainLogic
-                .Setup(x => x.GetResults(""))
-                .Throws(new WebException("404 - NotFound"));
-            //act
-            _consoleInterface.Start();
+                .Setup(x => x.GetResults(readLine))
+                .Throws(new WebException(writeLineRes));
             //assert
-            _consoleInOut.Verify(x => x.Write(writeLineRes), Times.Once);
-            _consoleInOut.Verify(x => x.Write(_writeLineOutput), Times.Once);
-            _consoleInOut.Verify(x => x.Read(), Times.Exactly(2));
+            Assert.Throws<WebException>(() => _consoleInterface.Start()).Message.Equals(writeLineRes);
         }
 
         [Test]
-        public void Start_InputCorrectURL_ReturnCorectCountWriteLine()
+        public void Start_InputCorrectUrl_ReturnCorectCountWriteLine()
         {
             //arrange
             var fakeURL = "https://example.com/";
-            var expectedURL = new List<string>()
+            var expectedUrl = new List<UrlModel>()
             {
-                "https://test.crawler.com/Info",
-                "https://test.crawler.com/main.html"
+                new UrlModel{Link ="https://test.crawler.com/Info",IsWeb=true},
+                new UrlModel{Link ="https://test.crawler.com/main.html",IsSitemap=true}
+            };
+            var expectedUrlWithTime = new List<UrlModelWithResponse>()
+            {
+                new UrlModelWithResponse{Link ="https://test.crawler.com/Info", TimeOfResponse=12},
+                new UrlModelWithResponse{Link ="https://test.crawler.com/main.html",TimeOfResponse=4}
             };
 
             _consoleInOut
@@ -101,11 +100,15 @@ namespace TestURLS.ConsoleApp.Tests
                 .Returns(fakeURL);
             _mainLogic
                 .Setup(x => x.GetResults(fakeURL))
-                .Returns(expectedURL);
+                .Returns(expectedUrl);
+            _mainLogic
+                .Setup(x => x.GetUrlsWithTimeResponse(expectedUrl))
+                .Returns(expectedUrlWithTime);
             //act
             _consoleInterface.Start();
             //assert
-            _consoleInOut.Verify(x => x.Write(expectedURL.First()), Times.Once);
+            _outputToConsole.Verify(x => x.WriteLinksWithoutTime(expectedUrl), Times.Once);
+            _outputToConsole.Verify(x => x.WriteLinksWithTime(expectedUrlWithTime), Times.Once);
             _consoleInOut.Verify(x => x.Write(_writeLineOutput), Times.Once);
         }
     }
